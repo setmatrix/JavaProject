cee4d8947fece0750ecda52fcf0da251f5593e9a
Daniel Kasprów <127341@student.uni.opole.pl>
2021-06-04 14:57:44 +0200

    Add Pane Rejestracja

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Welcome");
+			primaryStage.setTitle("Piszemy sobie program");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/application/WelcomeController.java
+++ src/application/WelcomeController.java
@@ -2,25 +2,41 @@ package application;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import javax.swing.JOptionPane;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import static javafx.fxml.FXMLLoader.*;
+
 public class WelcomeController {
-	
-	
+
+	@FXML
+	private BorderPane rootPane;
+
 	@FXML
 	private ImageView image1;
 	@FXML
+	
+	public ArrayList<Student> ListaUczniow = new ArrayList<Student>();
+	
 	public void initialize()
 	{
 		String zrodlo = System.getProperty("user.dir")+"\\bin\\obrazek.png";
@@ -33,23 +49,51 @@ public class WelcomeController {
 		}
 		Image image = new Image(inputstream);
 		image1.setImage(image);
-		//Daniel bóg
+		rootPane.setStyle("-fx-background-color:  #30C4CE;");
 	}
     @FXML
-    void login(ActionEvent event) {
+    void login(ActionEvent event) throws IOException {
     	try
     	{
-    		FXMLLoader logon = new FXMLLoader(getClass().getResource("Sample.fxml"));
-    		Parent root = (Parent) logon.load();
-    		Stage stage = new Stage();
-    		stage.setTitle("Log in");
-    		stage.setScene(new Scene(root));
-    		((Node)event.getSource()).getScene().getWindow().hide();
-    		stage.show();
+    		//FXMLLoader logon = new FXMLLoader(getClass().getResource("Sample.fxml"));
+    		//Parent root = (Parent) logon.load();
+    		//Stage stage = new Stage();
+    		//stage.setTitle("Log in");
+    		//stage.setScene(new Scene(root));
+    		//((Node)event.getSource()).getScene().getWindow().hide();
+    		//stage.show();
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			rootPane.setStyle("-fx-background-color:  #30C4CE;");
+
+			rootPane.getChildren().setAll(root);
+
+
     	}
     	catch (Exception e)
     	{
     		JOptionPane.showMessageDialog(null, e.getMessage(), "Login Window Exception", 0);
+    		
     	}
     }
+    @FXML
+    void rejestracjaAction(ActionEvent event) {
+    	try
+    	{
+    	BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Rejestracja.fxml"));
+		rootPane.setStyle("-fx-background-color:  #30C4CE;");
+
+		rootPane.getChildren().setAll(root);
+    	}
+    	catch (Exception e)
+    	{
+    		JOptionPane.showMessageDialog(null, e.getMessage(), "Welcome Window Exception", 0);
+    		
+    	}
+    }
+    
+    public static void rejestracjaUczia(String login, String haslo)
+    {
+    	//ListaUczniow.add(new Student(login,haslo));
+    }
+
 }
